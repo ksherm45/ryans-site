2d71d2ff02deac551976a350a30e578cc717bc7b
Kaj <kajbear@hotmail.com>
2023-10-31 15:05:55 +0100

    updated site for adaptable

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage/HomePage";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
-import SignupPage from "./pages/SignupPage/SignupPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
 import ServicesPage from "./pages/ServicesPage/ServicesPage";
 import ProfessionalCareerPage from "./pages/ProfessionalCareerPage/ProfessionalCareerPage";
 import CollegeCareerPage from "./pages/CollegeCareerPage/CollegeCareerPage";
@@ -14,8 +11,7 @@ import SmallGroupPage from "./pages/ServicesPage/SmallGroupPage";
 import LargeGroupPage from "./pages/ServicesPage/LargeGroupPage";
 
 import Navbar from "./components/Navbar/Navbar";
-import IsPrivate from "./components/IsPrivate/IsPrivate";
-import IsAnon from "./components/IsAnon/IsAnon";
+
 
 function App() {
   return (
@@ -39,33 +35,7 @@ function App() {
 
         <Route path="/largegroup" element={<LargeGroupPage />} />
 
-
-
-        <Route
-          path="/profile"
-          element={
-            <IsPrivate>
-              <ProfilePage />
-            </IsPrivate>
-          }
-        />
-
-        <Route
-          path="/signup"
-          element={
-            <IsAnon>
-              <SignupPage />
-            </IsAnon>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <IsAnon>
-              <LoginPage />
-            </IsAnon>
-          }
-        />
+       
       </Routes>
     </div>
   );
